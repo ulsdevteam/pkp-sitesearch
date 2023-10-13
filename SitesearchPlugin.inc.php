9e82aa905ad8be80e68f57f11d739c11371e01da
trp89 <trp89@pitt.edu>
2023-10-13 09:52:14 -0400

    Updates for OJS 3.4

--- SitesearchPlugin.inc.php
+++ SitesearchPlugin.inc.php
@@ -12,19 +12,21 @@
  * @brief Sitesearch plugin class
  */
 
-import('lib.pkp.classes.plugins.GenericPlugin');
+use PKP\plugins\GenericPlugin;
+use PKP\plugins\Hook;
+use PKP\core\PKPApplication;
 
 class SitesearchPlugin extends GenericPlugin {
 	
 	/**
 	 * @copydoc LazyLoadPlugin::register()
 	 */
-	function register($category, $path, $mainContextId = NULL) {
+	public function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path, $mainContextId);
 		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
 		if ($success && $this->getEnabled()) {
 			// Override the search templates
-			HookRegistry::register('TemplateManager::display', array($this, 'handleTemplateProcess'));
+			Hook::add('TemplateManager::display', array($this, 'handleTemplateProcess'));
 			}
 		return $success;
 	}
@@ -33,7 +35,7 @@ class SitesearchPlugin extends GenericPlugin {
 	 * Get the display name of this plugin.
 	 * @return String
 	 */
-	function getDisplayName() {
+	public function getDisplayName() {
 		return __('plugins.generic.sitesearch.displayName');
 	}
 
@@ -41,7 +43,7 @@ class SitesearchPlugin extends GenericPlugin {
 	 * Get a description of the plugin.
 	 * @return String
 	 */
-	function getDescription() {
+	public function getDescription() {
 		return __('plugins.generic.sitesearch.description');
 	}
 
@@ -49,21 +51,13 @@ class SitesearchPlugin extends GenericPlugin {
 	 * Hook callback: register output filter to modify the search form action
 	 * @see TemplateManager::display()
 	 */
-	function handleTemplateProcess($hookName, $args) {
+	public function handleTemplateProcess($hookName, $args) {
 		
 		$templateMgr = $args[0];
 		$template = $args[1];
-		/*
-		 * FIXME: this should really target just "frontend/pages/search.tpl" and "frontend/components/searchForm_simple.tpl"
-                 * But searchForm_simple is processed neither through the ::display or ::fetch hooks, so we are stuck with a global replace.
-		 */
 		if (strpos($template, 'frontend/pages/') === 0) {
 			if(method_exists($templateMgr, 'registerFilter')) {
-				// OJS 3.1.2 and later (Smarty 3)
 				$templateMgr->registerFilter("output", array($this, 'filterFormAction'));
-			} else {
-				// OJS 3.1.1 and earlier (Smarty 2)
-				$templateMgr->register_outputfilter(array($this, 'filterFormAction'));
 			}
 		}
 		return false;
@@ -75,12 +69,13 @@ class SitesearchPlugin extends GenericPlugin {
 	 * @param $templateMgr TemplateManager
 	 * @return $string
 	 */
-	function filterFormAction($output, $templateMgr) {
+	public function filterFormAction($output, $templateMgr) {
 		$request = $this->getRequest();
 		$router = $request->getRouter();
 		$dispatcher = $router->getDispatcher();
-		$contextSearch = $dispatcher->url($request, ROUTE_PAGE, null, 'search');
-		$siteSearch = $dispatcher->url($request, ROUTE_PAGE, 'index', 'search');
+		$routePage = PKPApplication::ROUTE_PAGE;
+		$contextSearch = $dispatcher->url($request, $routePage, null, 'search');
+		$siteSearch = $dispatcher->url($request, $routePage, 'index', 'search');
 		$output = str_replace($contextSearch, $siteSearch, $output);
 		return $output;
 	}
@@ -89,17 +84,14 @@ class SitesearchPlugin extends GenericPlugin {
 	/**
 	 * @see PKPPlugin::getTemplatePath()
 	 */
-	function getTemplatePath($inCore = false) {
+	public function getTemplatePath($inCore = false) {
 		$templatePath = parent::getTemplatePath($inCore);
-		// OJS 3.1.2 and later include the 'templates' directory, but no trailing slash
 		$templateDir = 'templates';
 		if (strlen($templatePath) >= strlen($templateDir)) {
 			if (substr_compare($templatePath, $templateDir, strlen($templatePath) - strlen($templateDir), strlen($templateDir)) === 0) {
 				return $templatePath;
 			}
 		}
-		// OJS 3.1.1 and earlier includes a trailing slash to the plugin path
-		return $templatePath . $templateDir . DIRECTORY_SEPARATOR;
 	}	
 
 }
